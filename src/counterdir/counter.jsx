a349519491db64b52f0fb3c7a4a1b026bbe54d69
Biplov Pokhrel <pbplop@gmail.com>
2020-03-07 13:31:27 +0530

    just saving my bootstrap learning

---
+++ src/counterdir/counter.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="text-right">
+        <h1 className=" text-right btn btn-block btn-warning btn-sm">Hello</h1>
+        <button className=" btn-warning disabled">Increase</button>
+        <button className=" btn-outline-info btn-lg ">Decrease</button>
+        <badge className="btn btn-block btn-success text-left">success</badge>
+      </div>
+    );
+  }
+}
+
+export default Counter;
