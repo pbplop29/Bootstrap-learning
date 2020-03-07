d9e40f3922f9d890e6650caca696f272e0e2d590
Biplov Pokhrel <pbplop@gmail.com>
2020-03-08 03:24:16 +0530

    Learnt about tagging and rendering lists

--- src/counterdir/counter.jsx
+++ src/counterdir/counter.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    Roll: 15
+    Roll: 15,
+    horny: ["ram1", "ram2", "ram3"]
   };
   render() {
     // this.colorBadge();
 
     return (
-      <div className="text-right">
+      <div>
+        {/* 
         <span className={this.colorBadge()}>{this.getRollItSelf()}</span>
         <h1 className="btn btn-outline-warning">Niyeti</h1>
         <button className=" btn-warning disabled">Increase</button>
@@ -15,6 +17,14 @@ class Counter extends Component {
         <badge className="btn btn-block btn-success text-left">
           success duihhh!!!!
         </badge>
+        <ul>
+          {this.state.horny.map(tag => (
+            <li key={tag}>
+              <h1 className="btn btn-warning">{tag}</h1>
+            </li>
+          ))}
+        </ul>
+         */}
       </div>
     );
   }
