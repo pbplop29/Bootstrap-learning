14f590ff3cf2a3e1e8cf7c4313219c982363d6e1
Biplov Pokhrel <pbplop@gmail.com>
2020-03-07 13:37:26 +0530

    Just trying out if the wuote applies to everything or this file only

--- src/counterdir/counter.jsx
+++ src/counterdir/counter.jsx
@@ -7,7 +7,9 @@ class Counter extends Component {
         <h1 className=" text-right btn btn-block btn-warning btn-sm">Hello</h1>
         <button className=" btn-warning disabled">Increase</button>
         <button className=" btn-outline-info btn-lg ">Decrease</button>
-        <badge className="btn btn-block btn-success text-left">success</badge>
+        <badge className="btn btn-block btn-success text-left">
+          success duihhh!!!!
+        </badge>
       </div>
     );
   }
