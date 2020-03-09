fc65622dda1131b299d0e8f3974e9feca4a5c7dc
Biplov Pokhrel <pbplop@gmail.com>
2020-03-09 17:48:24 +0530

    Commented the important stuff and done

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./counterdir/counter";
-
-ReactDOM.render(<Counter />, document.getElementById("root"));
+import Counters from "./counterdir/counters";
+//Here I just imported the ocunters jsx file which i s supposed to be the module file containing multiple no of counter jsx files which basically increases the number of displays present in counter.jsx
+//Earlier i had counter fiule only and not counters file because i was not using multiple ones. But since i am including multiple of them i had to create a different jsx file and then import and render it.
+ReactDOM.render(<Counters />, document.getElementById("root"));
+//Here i am basically rendering the counters file that i imported.
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
