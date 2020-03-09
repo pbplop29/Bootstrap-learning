0ec64e111b8cff93c3643e37f51f9eb4c893ed10
Biplov Pokhrel <pbplop@gmail.com>
2020-03-09 17:46:12 +0530

    Added multiple comment lines that will explain the basic foundation of this program

--- src/counterdir/counter.jsx
+++ src/counterdir/counter.jsx
@@ -1,43 +1,48 @@
+//This is the most primitive building block file that is used in other jsx files and then imported in js files to be executed.
+
 import React, { Component } from "react";
 class Counter extends Component {
+  //defined a class counter
   state = {
-    count: 0
+    value: this.props.value
+    //made use of the props feature so as to assign a value to the variable value which is given by the state of counters.jsx  file.
+    //Line 22 col 10 of counters.jsx
+    //<Counter key={counter.id} ///value///={counter.value}>
+    //the value between /// /// is the value used here. because if we change the value as any variable lets say X={counter.value},, we'll do value:this.props.X
   };
-  //constructor() {
-  // super();
-  // this.clickI = this.clickI.bind(this);
-  //}
   clickI = product => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
+    //stated a function to increase the value by on that will be used as a className --> onClick for the event of clicking the increase button
   };
   clickD = () => {
-    this.setState({ count: this.state.count - 1 });
+    return this.state.value > 0
+      ? this.setState({ value: this.state.value - 1 })
+      : "Nothing";
+    //stated a function to decrease the value by on that will be used as a className --> onClick for the event of clicking the decrease button
+    //also added a condition such that the value displayed will never go below zero.
+    //I still have confusion with how the "Nothing" part is working but i will figure out soon about the alerts and stuff and put it there such that if it goes below xero it will show an alert message.
   };
-  heCounts() {
-    return this.state.count === 0 ? "Zero" : this.state.count;
+  hevalues() {
+    return this.state.value === 0 ? "Zero" + " kg" : this.state.value + " kg";
+    //stated a function to give a output of the value + " kg" st it will give "Zero" if valye ===0 or else it will return the present value given by this.state.value
   }
   colorCorrection() {
-    return this.state.count < 0
+    return this.state.value <= 0
       ? "btn-block text-warning badge-danger"
       : "btn-block text-light badge-success";
+    //stated a function to determine the color of the badge such that if its negative value it gives a diff value and diff for positive or zero
   }
-  //so comments
-  //cannot
-  //be
-  //added
-  //directly
-  //wherever
-  //but i will try
 
   render() {
     return (
       <div className="text-center">
-        <span className="text=danger btn-block btn-info btn-disable">
-          So this will be the heading and below it will be the buttons that will
-          focus onm increment and stuff
+        {this.props.children}
+        <span className="text-warning badge-block badge-link badge-lg">
+          Click increase to increase the kilograms of product you will buy.
+          &nbsp; &nbsp; &nbsp;&nbsp;Click Decrease to decrease the kilograms of
+          product you will buy.
         </span>
-        <span className={this.colorCorrection()}>{this.heCounts()}</span>
+        <span className={this.colorCorrection()}>{this.hevalues()}</span>
         <button
           className="btn btn-success text-warning "
           onClick={() => this.clickI({ id: 1 })}
@@ -49,6 +54,16 @@ class Counter extends Component {
           Decrease
         </button>
       </div>
+      //in div --> aligned everything at center
+      //{this.props.children} is the result of having jsx command in between the counter module inserted between the <div> of counters.jsx file.
+      //            <Counter key={counter.id} value={counter.value}>
+      //            <h1>Counter {counter.id}</h1>                                     --------->basically this instance
+      //      in case i had <h1> yoyohoneysingh </h1> here ,,, the use of {this.props.children} would have inserted yoyohoneysingh everywhere it appeared.
+      //            </Counter>
+      //1st <span> is nothing but a badge to message about the functionality
+      //2nd <span> just uses the colorCorrection() function and the heValues() functions to give the output of value with correspoding color
+      //<button> these are given with classnames for appearance and onclicks for the event handling
+      //&nsbp; provides spaces between buttons by a small margin
     );
   }
 }
